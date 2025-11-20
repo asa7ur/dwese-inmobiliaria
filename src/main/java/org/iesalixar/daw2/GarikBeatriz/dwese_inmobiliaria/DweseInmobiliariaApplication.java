8c4046a51f80acb6f79b88253ae1d97c79482923
Garik Asatryan <asa7ur@proton.me>
2025-11-20 15:33:46 +0100

    Initial commit

---
+++ src/main/java/org/iesalixar/daw2/GarikBeatriz/dwese_inmobiliaria/DweseInmobiliariaApplication.java
@@ -0,0 +1,13 @@
+package org.iesalixar.daw2.GarikBeatriz.dwese_inmobiliaria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DweseInmobiliariaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DweseInmobiliariaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/iesalixar/daw2/GarikBeatriz/dwese_inmobiliaria/DweseInmobiliariaApplicationTests.java
@@ -0,0 +1,13 @@
+package org.iesalixar.daw2.GarikBeatriz.dwese_inmobiliaria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DweseInmobiliariaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
