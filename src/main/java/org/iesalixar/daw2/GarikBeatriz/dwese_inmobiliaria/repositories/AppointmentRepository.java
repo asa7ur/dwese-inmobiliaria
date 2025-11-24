1ea85158e20adf785ec7f710d8aef416e711667f
bbarper657 <beatrizbarrachina85@gmail.com>
2025-11-24 16:28:50 +0200

    Creación de los controladores de Office, Appointment y Property, y actualización de los repositorios de Appointment y Property.

--- src/main/java/org/iesalixar/daw2/GarikBeatriz/dwese_inmobiliaria/repositories/AppointmentRepository.java
+++ src/main/java/org/iesalixar/daw2/GarikBeatriz/dwese_inmobiliaria/repositories/AppointmentRepository.java
@@ -1,8 +1,10 @@
 package org.iesalixar.daw2.GarikBeatriz.dwese_inmobiliaria.repositories;
 
+import org.iesalixar.daw2.GarikBeatriz.dwese_inmobiliaria.entities.Appointment;
 import org.springframework.data.jdbc.repository.query.Query;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface AppointmentRepository {
+public interface AppointmentRepository extends JpaRepository<Appointment, Long> {
 
     @Query("SELECT COUNT(a) > 0 FROM appointments a WHERE a.code =? :code")
     boolean existsAppointmentByCode(String code);
