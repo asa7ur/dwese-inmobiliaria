26f9b1c613a9805a18c93e5ffcea7c58a5116f5f
Garik Asatryan <asa7ur@proton.me>
2025-12-01 16:15:41 +0100

    Adding the necessary code for image uploading

--- src/main/java/org/iesalixar/daw2/GarikBeatriz/dwese_inmobiliaria/config/WebConfig.java
+++ src/main/java/org/iesalixar/daw2/GarikBeatriz/dwese_inmobiliaria/config/WebConfig.java
@@ -2,23 +2,37 @@ package org.iesalixar.daw2.GarikBeatriz.dwese_inmobiliaria.config;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
     private static final Logger logger = LoggerFactory.getLogger(WebConfig.class);
 
+    @Value("${UPLOAD_PATH}")
+    private String uploadPath;
+
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        String uploadPath = System.getProperty("UPLOAD_PATH");
         if (uploadPath != null && !uploadPath.isEmpty()) {
-            logger.info("UPLOAD_PATH configurado correctamente: {}", uploadPath);
+            // Convertimos la ruta relativa "uploads" a una ruta absoluta del sistema
+            // y luego a URI (esto añade automáticamente el prefijo file:/// correcto)
+            Path path = Paths.get(uploadPath).toAbsolutePath().normalize();
+            String resourcePath = path.toUri().toString();
+
+            logger.info("Configurando recursos estáticos.");
+            logger.info("Carpeta física: {}", path);
+            logger.info("URI mapeada: {}", resourcePath);
+
             registry.addResourceHandler("/uploads/**")
-                    .addResourceLocations("file:" + uploadPath + "/");
+                    .addResourceLocations(resourcePath);
         } else {
-            logger.error("La variable de entorno UPLOAD_PATH no está configurada o está vacía.");
+            logger.error("La variable UPLOAD_PATH no está configurada o está vacía.");
         }
     }
 }
