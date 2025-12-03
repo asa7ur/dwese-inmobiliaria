924908ae1fa41c10b50527a4418218687bfb95cd
Garik Asatryan <asa7ur@proton.me>
2025-12-03 18:04:59 +0100

    Added code for multiple property photo view

---
+++ src/main/java/org/iesalixar/daw2/GarikBeatriz/dwese_inmobiliaria/utils/ImageUtils.java
@@ -0,0 +1,20 @@
+package org.iesalixar.daw2.GarikBeatriz.dwese_inmobiliaria.utils;
+
+import org.iesalixar.daw2.GarikBeatriz.dwese_inmobiliaria.entities.PropertyImage;
+import org.springframework.stereotype.Component;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Component("imgHelper") // El nombre "imgHelper" es como lo llamaremos en el HTML
+public class ImageUtils {
+
+    public String getImagesAsString(List<PropertyImage> images) {
+        if (images == null || images.isEmpty()) {
+            return "";
+        }
+        // Convierte la lista de objetos Image a un String separado por comas
+        return images.stream()
+                .map(PropertyImage::getFileName)
+                .collect(Collectors.joining(","));
+    }
+}
